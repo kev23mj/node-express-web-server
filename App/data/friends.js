0bbf739770b454ea87cdc09a7908e05f441822c1
kev23mj <kevinsmartabbey@gmail.com>
2017-07-07 05:46:15 -0700

    updated

---
+++ App/data/friends.js
@@ -0,0 +1,13 @@
+var friends = [
+  { name: 'Frank',
+  photo: 'http://images.askmen.com/top_10/entertainment/1267463242_top-10-crime-bosses_8.jpg',
+  scores: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ] },
+
+  { name: 'Tony',
+  photo: 'https://upload.wikimedia.org/wikipedia/en/1/19/Scarfaceinthefall.jpg',
+  scores: [ '3', '3', '3', '3', '3', '3', '3', '3', '3', '3' ] },
+
+  { name: 'Tommy',
+  photo: 'http://por-img.cimcontent.net/cms/data/assets/bin-201408/a821e7eadf58e26cf84cb6a5ebbc723d_600x400.jpg',
+  scores: [ '5', '5', '5', '5', '5', '5', '5', '5', '5', '5' ] }
+];
